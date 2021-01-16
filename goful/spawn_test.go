1788e457e388e234b75c4ead3407e2ca79791583
anmitsu <anmitsu.s@gmail.com>
2021-01-17 08:28:30 +0900

    test: add expand macro test for spawn

---
+++ goful/spawn_test.go
@@ -0,0 +1,41 @@
+package goful
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestExpandMacro(t *testing.T) {
+	g := New("")
+	g.Workspace().ReloadAll() // in home directory
+
+	home, _ := os.UserHomeDir()
+	macros := []struct {
+		in  string
+		out string
+	}{
+		{`%f`, `".."`},
+		{`%F`, fmt.Sprintf(`"%s"`, filepath.Dir(home))},
+		{`%x`, `"."`},
+		{`%X`, fmt.Sprintf(`"%s"`, filepath.Dir(home))},
+		{`%m`, `".."`},
+		{`%M`, fmt.Sprintf(`"%s"`, filepath.Dir(home))},
+		{`%d`, fmt.Sprintf(`"%s"`, filepath.Base(home))},
+		{`%D`, fmt.Sprintf(`"%s"`, home)},
+		{`%d2`, fmt.Sprintf(`"%s"`, filepath.Base(home))},
+		{`%D2`, fmt.Sprintf(`"%s"`, home)},
+		{`%%%f`, `%%".."`},
+		{`\%f%f`, `%f".."`},
+		{`%\f%f`, `%f".."`},
+		{"%AA%ff", `%AA".."f`},
+	}
+
+	for _, macro := range macros {
+		ret, _ := g.expandMacro(macro.in)
+		if ret != macro.out {
+			t.Errorf("%s -> %s result %s\n", macro.in, macro.out, ret)
+		}
+	}
+}
