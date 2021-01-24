dc4a370d37b19a9fb0d7f8d6a961dfa831600194
anmitsu <anmitsu.s@gmail.com>
2021-01-24 10:13:50 +0900

    feat: add disk space information for windows

--- infobar/infobar_windows.go
+++ infobar/infobar_windows.go
@@ -5,8 +5,11 @@ package infobar
 import (
 	"fmt"
 	"os"
+	"syscall"
+	"unsafe"
 
 	"github.com/anmitsu/goful/look"
+	"github.com/anmitsu/goful/utils"
 	"github.com/anmitsu/goful/widget"
 	"github.com/mattn/go-runewidth"
 )
@@ -20,7 +23,18 @@ func (w *infoWindow) draw(fi os.FileInfo) {
 	mtime := fi.ModTime().String()
 	name := fi.Name()
 
-	info := fmt.Sprintf("%s %d %s %s", perm, size, mtime, name)
+	h := syscall.MustLoadDLL("kernel32.dll")
+	c := h.MustFindProc("GetDiskFreeSpaceExW")
+	var free, all int64
+	_, _, _ = c.Call(uintptr(
+		unsafe.Pointer(syscall.StringToUTF16Ptr("."))),
+		uintptr(unsafe.Pointer(&free)),
+		uintptr(unsafe.Pointer(&all)),
+		uintptr(unsafe.Pointer(nil)))
+	used := float64(all-free) / float64(all) * 100
+	freeSI := utils.FormatSize(free)
+
+	info := fmt.Sprintf("%s free (%.1f%s used) %s %d %s %s", freeSI, used, "%", perm, size, mtime, name)
 	s := runewidth.Truncate(info, w.Width(), "...")
 	widget.SetCells(x, y, s, look.Default())
 }
